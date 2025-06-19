9302116651d40e188dfeff4203fe21f9bbff80dd
Igor <igorberaldomo@gmai.com>
2025-06-19 16:01:14 -0300

    1.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet,  } from '@angular/router';
 import { TitleComponent } from "./components/title/title.component";
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TitleComponent],
+  imports: [RouterOutlet, TitleComponent, SmallCardComponent, BigCardComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
