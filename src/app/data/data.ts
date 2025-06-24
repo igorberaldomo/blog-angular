76b9426478002a1740aa7b57a7175d030be24eb1
Igor <igorberaldomo@gmai.com>
2025-06-24 17:16:58 -0300

    1.6.0

---
+++ src/app/data/data.ts
@@ -0,0 +1,26 @@
+export const Data = [
+    {
+        id: "1",
+        title: "O novo headphone da apple foi lançado",
+        text: "Veja como a qualidade do som foi melhorada, não perca essa oportunidade!",
+        image: "/assets/images/headphone.jpeg",
+    },
+    {
+        id:"2",
+        title: "Javascript 21 lançado",
+        text: "Nessa terça feira dia 18 de maio de 2022, o javascript 21 foi lancado",
+        image: "/assets/images/javascript.png",
+    },
+    {
+        id: "3",
+        title: "Novo exploit encontrado!",
+        text: "foi encontrado um exploit no mysql versão 5.7",
+        image: "/assets/images/coding.jpeg",
+    },
+    {
+        id: "4",
+        title: "Teclado Dinamico",
+        text: "veja essa novo teclado dinâmico que foi desenvolvido para facilitar a vida dos programadores",
+        image: "/assets/images/keyboard.jpeg",
+    }
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import {ContentTitleComponent} from '../../components/content-title/content-title.component';
-import {ContentTextComponent} from '../../components/content-text/content-text.component';
+import { ContentTitleComponent } from '../../components/content-title/content-title.component';
+import { ContentTextComponent } from '../../components/content-text/content-text.component';
+import { ActivatedRoute } from '@angular/router';
+import { Data } from "../../data/data"
 
 @Component({
   selector: 'app-content',
@@ -10,10 +12,25 @@ import {ContentTextComponent} from '../../components/content-text/content-text.c
   styleUrl: './content.component.css'
 })
 export class ContentComponent implements OnInit {
+  text: string = '';
+  title: string = '';
+  private id: string | null = '0';
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value => {
+      this.id = value.get('id')
+    })
+    this.setValuesToComponent(this.id);
   }
 
+  setValuesToComponent(id: string | null) {
+    const array = Data.filter(value => value.id == id)[0]
+    this.title = array.title;
+    this.text = array.text;
+  
+  }
 }
